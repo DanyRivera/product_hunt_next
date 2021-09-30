79edda289fe4c95828113d10ed0ee37804d71e44
Dany <dany@gmail.com>
2021-09-30 18:10:12 -0500

    Empezando

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react'
+
+const Layout = (props) => {
+
+    return ( 
+
+        <Fragment>
+
+            <h1>Header</h1>
+
+            <main>
+                {props.children}
+            </main>
+
+        </Fragment>
+     
+    );
+}
+ 
+export default Latout;
