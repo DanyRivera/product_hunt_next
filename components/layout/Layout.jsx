4886b10624b0f1df452b26cd1903f1bbe9dfce25
Dany <dany@gmail.com>
2021-10-05 20:42:29 -0500

    Finalizando Sidebar

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -56,13 +56,17 @@ const Layout = (props) => {
                         text-decoration: none;
                     }
 
+                    img {
+                       max-width: 100%;
+                    }
+
                 `}
             />
 
             <Head>
 
                 <title>Product Hunt Firebase y Next.js</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
                 <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet"/>
                 <link rel="stylesheet" href="/static/css/app.css" />
 
